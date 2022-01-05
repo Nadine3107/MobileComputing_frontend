984298f6813ef8be1a75c622017584ab01512223
nadine <na.krautscheid@gmail.com>
2022-01-05 12:36:15 +0100

    test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>OHallo zusammen</Text>
+      <Text>OHallo zusammenTEST</Text>
       <StatusBar style="auto" />
     </View>
   );
