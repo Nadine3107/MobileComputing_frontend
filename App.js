3271b6e898206c4876f0827023e5df2725085f21
Melina <melina.classen@stud.hs-ruhrwest.de>
2022-01-06 16:18:12 +0100

    Pfad zum Impressum angepasst

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Impressum from './Impressum/Impressum';
+import Impressum from './Screens/Impressum/Impressum';
 
 export default function App() {
   return (
